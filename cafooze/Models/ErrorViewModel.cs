39e9578c23e001e656c216a36ab63f3c4ed31311
Admin <Admin@DESKTOP-29H0PRD>
2023-07-12 20:20:52 -0700

    Add project files.

---
+++ cafooze/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cafooze.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
